bef451587043a5a09491114e7af8831c530736a5
Dinn Eferet <dinn.eferet@gmail.com>
2024-09-04 09:08:04 +0100

    Patches

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # compare-spreadsheets.py
 
-A python program for comparing spreadsheets. Currently compares data in 2 Excel
+A python program for comparing spreadsheets. Currently compares data in two Excel
 files (paths specified at prompt) and puts all missing ID cells in a third file
-called ```missing.xlsx``` (located in ```./assets```).
+(called ```missing.xlsx```).
 
 
 
 ## Requires
 
-Python 3+
+[Python] (https://www.python.org/downloads/ 'Download Python')
 
 
 ## Run
 
-Download **compare-spreadsheets** folder. Open folder in terminal. Type ```./run.sh```. Enjoy!
+Download **compare-spreadsheets** folder. Open folder in your Terminal/Command Prompt.
+Type ```./run.sh```. Enjoy!
 
 
 _More usefulness coming soon!_
--- compare-spreadsheets.py
+++ compare-spreadsheets.py
@@ -1,25 +1,28 @@
 '''
-================================================================================
+--------------------------------------------------------------------------
     Name:         compare-spreadsheets.py
 
     Author:       Barra Hart
 
     Repo:         https://github.com/barrahart/compare-spreadsheets
 
-    Description:  Compared identifying columns in 2 Excel files and puts
-                  all missing IDs in a third.
-================================================================================
+    Description:  Compares ID columns in two Excel files and puts
+                  all IDs not in both files in a third file.
+--------------------------------------------------------------------------
 '''
 
-from openpyxl import *
+from openpyxl import * # library for working with Excel files
 
+# fancy
 def line():
-    print('\n---------------------------------------------------------------------\n')
+    print('---------------------------------------------------------------------\n')
 
 
 def compare(projectsA, projectsB, missingProjectsArray, missingProjects):
-    #print(f"FIRST ID: {projectsA['A1'].value}")
 
+    # fancy
+    print("\n\n")
+    # compare ID column in file 1 against each ID in file 2
     for projectA in projectsA.iter_rows(values_only=True):
         flag=False
 
@@ -27,37 +30,42 @@ def compare(projectsA, projectsB, missingProjectsArray, missingProjects):
             if projectA[0]==projectB[0]:
                 flag=True
 
-        #print(f"Done with search for ID {projectA[0]}\n")
+        # notify user of progress
+        print(f"> Done with search for ID {projectA[0]}\n")
 
-        if flag==False:
+        if flag==False: # ID not in second file
             missingProjectsArray.append(projectA[0])
 
+    # update file for missing IDs
     for i in range(len(missingProjectsArray)):
         missingProjects[f'A{i+1}'] = missingProjectsArray[i]
 
 
-
+# fancy
 print('\n---------------------------------------------------------------------')
 print("::::::::::::::::::::: Compare Spreadsheets 1.0 ::::::::::::::::::::::")
 print('---------------------------------------------------------------------')
 user_continue=input(":::::::::::: Press ENTER to continue, or CTRL-C to exit :::::::::::::\n")
 
 line()
+# import 1st file
 user_work1 = input("\n> Enter full path for Spreadsheet 1: ")
 workbook1 = load_workbook(f'{user_work1}')
 sheet1=workbook1.worksheets[0]
 
+# import 2nd file
 user_work2 = input("\n> Enter full path for Spreadsheet 2: ")
 workbook2 = load_workbook(f'{user_work2}')
 sheet2=workbook2.worksheets[0]
 
+# import 3rd file
 workbook3 = load_workbook('./missing.xlsx')
 sheet3 = workbook3.worksheets[0]
 
+# temporary storage for missing IDs
 missing=[]
 
 
-line()
 
 compare(sheet1, sheet2, missing, sheet3)
 
@@ -69,7 +77,9 @@ for id in missing:
 line()
 '''
 
+# save changes to 3rd file
 workbook3.save('./missing.xlsx')
 
-print("\n> Done. Check 'missing.xlsx' for your missing IDs!")
+# notification
+print("\n\n> Done. Check 'missing.xlsx' for your missing IDs!")
 line()
